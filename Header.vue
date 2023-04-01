9b59afdd5ab34258a23805de92ae3d12ac204a4d
janus2k1 <95867041+janus2k1@users.noreply.github.com>
2023-04-01 04:06:25 +0200

    Add files via upload

---
+++ Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-title>
+      <a href="/public" style="text-decoration: none; color: inherit;">FreeWMS</a>
+    </v-app-bar-title>
+    <v-app-bar-title>Dostawy --> Magazynowanie --> Wysyłki</v-app-bar-title>
+    <v-app-bar-title>{{ currentDate }}</v-app-bar-title>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  name: "HeaderComponent",
+  data() {
+    return {
+      currentDate: this.getCurrentDateTime()
+    }
+  },
+  created() {
+    setInterval(() => {
+      this.currentDate = this.getCurrentDateTime();
+    }, 1000);
+  },
+  methods: {
+    getCurrentDateTime() {
+      const now = new Date();
+      return now.toLocaleString();
+    }
+  }
+}
+</script>
+<style>
+</style>

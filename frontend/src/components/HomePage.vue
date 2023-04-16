b851a9fb0e62238452e3cb6cf75708d38105254d
michaldabrowski98 <mdboxman@gmail.com>
2023-04-16 08:45:23 +0200

    Fix in README.md removing sass

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -93,9 +93,5 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
-.v-card.on-hover.v-theme--dark
-  background-color: rgba(#FFF, 0.8)
-  >.v-card__text
-    color: #000
+<style scoped>
 </style>
